87383a24d1eb805745d26c84e5c7b86f94edbd04
sherpatsonam <sonam.sherpa2018@outlook.com>
2019-04-25 10:23:09 -0700

    Controllers

---
+++ Evaluation/Controllers/DeptHeadController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Evaluation.Controllers
+{
+    public class DeptHeadController : Controller
+    {
+        // GET: DeptHead
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
