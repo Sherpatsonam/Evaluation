3aa6bd77f1c86ebfb5e86cf25036e3b2cc1c6bb2
Dominic <shovelware@protonmail.ch>
2019-05-01 11:57:34 -0700

    associate details view with teacherid

--- Evaluation/Controllers/TeacherController.cs
+++ Evaluation/Controllers/TeacherController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Evaluation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,30 @@ namespace Evaluation.Controllers
 {
     public class TeacherController : Controller
     {
-        // GET: Teacher
+        private EvaluationDB db = new EvaluationDB();
+        // POST: Teacher
+        // Select a teacher ID to determine which evaluation to show
         public ActionResult Index()
         {
             return View();
+        }
+
+        // GET: Evaluations
+        // Show a teacher's evaluation results
+        public ActionResult Details(int? id)
+        {
+            if (id==null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // request a complete evaluation form based on teacherID
+            Evaluation.Models.Evaluation eval = db.Evaluations.Where(x => x.TeacherID == id).FirstOrDefault();
+            if (eval == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eval);
         }
     }
 }
