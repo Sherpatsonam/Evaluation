f1075f2dac4af5b93ca2adbebdbc14b0f63d1e3c
sherpatsonam <sonam.sherpa2018@outlook.com>
2019-05-02 20:32:05 -0700

    working

--- Evaluation/Controllers/TeacherController.cs
+++ Evaluation/Controllers/TeacherController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Evaluation.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,10 +10,38 @@ namespace Evaluation.Controllers
 {
     public class TeacherController : Controller
     {
-        // GET: Teacher
-        public ActionResult Index()
+        private EvaluationDB db = new EvaluationDB();
+        // POST: Teacher
+        // Select a teacher ID to determine which evaluation to show
+        public ActionResult Index(User user)
         {
-            return View();
+            if (user.username == "teacher")
+            {
+                return View();
+            }
+            return RedirectToAction("../Login/Login");
+        }
+
+        // GET: Evaluations
+        // Show a teacher's evaluation results
+        public ActionResult Details(int? teacherid, int? std, string graded)
+        {
+            if (graded != "yes")
+            {
+                return View("Error");
+            }
+            if (teacherid==null && std == null) 
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            // request a complete evaluation form based on teacherID
+            Evaluation.Models.Evaluation eval = db.Evaluations.Where(x => x.TeacherID == teacherid && x.StudentID==std).FirstOrDefault();
+            if (eval == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eval);
         }
     }
 }
